0ebbb42aa22edd4540c7ad24a1bcd747618f0cab
Lukasz Odulinski <lukasz@videolicious.com>
2019-03-24 10:53:53 +0100

    solution

--- sunday_warmup/src/main/java/Warmup.java
+++ sunday_warmup/src/main/java/Warmup.java
@@ -13,6 +13,10 @@ class Product {
         this.name = name;
         this.priceInPln = priceInPln;
     }
+
+    public double getPrice() {
+        return priceInPln;
+    }
 }
 
 class Warmup {
@@ -35,17 +39,37 @@ class Warmup {
     }
 
     private static List<Product> filterPremium(List<Product> products) {
-        //napisz implementację metody tak by zwrócona lista zawierała tylko produkty droższe niż 3000 PLN
-        return products;
+        //napisz implementację metody tak by zwrócona lista zawierała
+        // tylko produkty droższe niż 3000 PLN
+        /*List<Product> listOfPremiumProducts = new ArrayList<>();
+        for(Product product : products){
+            if(product.getPrice() > 3000.00){
+                listOfPremiumProducts.add(product);
+            }
+        }*/
+        return products.stream()
+            .filter(p -> p.getPrice() > 3000)
+            .collect(Collectors.toList());
     }
 
     private static void task2() {
-        //Napis implementacje dla każdej lambdy tak by kod się kompilował
-        /*Function<Integer, String> itos = ;
-        Function<List<String>, Integer> sizeOfList = ;
-        Function<String, Boolean> isEmpty = ;
-        Function<Warmup, Long> getValue = ;
-        Function<String, String> prefix = ;
-        Function<Integer, List<Integer>> listOf = ;*/
+        //Napisz implementacje dla każdej lambdy tak by kod się kompilował
+        // argument -> implementacja
+        //Function<R,Boolean> == Predicate<R>
+        Function<Integer, String> itos = i -> "" + i;
+        Function<List<String>, Integer> sizeOfList = l -> l.size();
+        Function<String, Boolean> isEmpty = s -> s.isEmpty();
+        Function<Warmup, Long> getValue = w -> w.value;
+        Function<String, String> prefix = s -> "prefix" + s + "_";
+        Function<Integer, List<Integer>> listOf = i -> {
+            return IntStream.range(1, i + 1)
+                .boxed()
+                .collect(Collectors.toList());
+        };
+
+        Arrays.asList(1, 2, 3, 4).stream() // produces list of 4 ints [1,2,3,4]
+            .map(i -> itos.apply(i))
+            .map(s -> prefix.apply(s))
+            .forEach(i -> System.out.println(i));
     }
 }
