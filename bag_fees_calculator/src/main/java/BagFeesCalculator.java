7de40c1cfad85795ffc73bccbbe17f837b43ab4f
Lukasz Odulinski <lukasz@videolicious.com>
2019-03-29 22:02:33 +0100

    bagfees

---
+++ bag_fees_calculator/src/main/java/BagFeesCalculator.java
@@ -0,0 +1,15 @@
+public class BagFeesCalculator {
+    //Na lotnisku pracuje pani Asia
+    //Przychodzą do niej ludzie z biletami na klasy
+    //ekonomiczną, biznes, first class
+    //pasażer z biletem na ekonomiczną może zabrać 1 walizkę do 23kg bez dopłat
+    //pasażer z biletem na biznes klasę może zabrać 2 walizki do 23kg bez dopłat
+    //pasażer z biletami na pierwszą klasę może zabrać 1 walizkę do 32kg i 1 do 23 bez dopłat
+    //1kg ponad dozwolone kosztuje 25 zł
+    //napisz metodę która na podstawie przedstawionego biletu i ilości walizek
+    //obliczy opłatę za bagaż, jeśli brak opłat to metoda powinna zwracać 0
+    //dodatkowo pasażer może przedstawić do biletu kartę frequent flyera
+    //karty są 3 Bronze, Silver i Gold. Pierwsza daje 10% zniżki na
+    // opłaty, Silver daje 20% zniżki a Gold 35%
+    //uaktualnij kalkulacje tak by akceptowała karty
+}
