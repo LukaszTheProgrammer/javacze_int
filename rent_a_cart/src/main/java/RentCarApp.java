84818bf0ee820db557837ea7982b3635ca980ef5
Lukasz Odulinski <lukasz@videolicious.com>
2019-04-06 08:36:56 +0200

    week3 tasks

---
+++ rent_a_cart/src/main/java/RentCarApp.java
@@ -0,0 +1,17 @@
+public class RentCarApp {
+
+    public static void main(String[] args) {
+        /*
+        Napisz klasę CarRental która będzie przechowywać listę samochodów.
+        Klasa powinna udostępniać następujące metody:
+        - listCars - zwracającą listę samochodów
+
+        - rentCar - pozwalającą wypożyczyć auto na pewien okres
+                    metoda powinna nie pozwalać wypożyczyć auta
+                    które już zostało wypożyczone
+
+        Przeciąż metodę tak by można było wyszukiwać auta
+        dostępne w danym przedziale czasowym
+         */
+    }
+}
---
+++ todo/src/main/java/Todo.java
@@ -0,0 +1,16 @@
+public class Todo {
+
+    public static void main(String[] args) {
+        /*
+        Napisz klasę TaskList, która będzie przechowywać
+        listę zadań, zadanie powinno mieć opis oraz flagę
+        która mówi czy jest wykonane czy nie.
+        Klasa TaskList powinna udostępniać następujące funkcjonalności:
+        - dodawanie zadania
+        - oznaczenie zadania jako wykonane
+        - pobranie wszystkich zadań
+        - pobranie zadań wykonanych
+        - pobranie zadań niewykonanych
+         */
+    }
+}
